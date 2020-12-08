918205984773a3dbae7a76597e788ab1dab8b23b
Shlok Kaneria <32154972+skaneri1@users.noreply.github.com>
2020-12-08 11:55:05 -0500

    Create BoolVal.java

---
+++ FINAL PROJECT/proj2-skaneri1/BoolVal.java
@@ -0,0 +1,9 @@
+public class BoolVal extends Value {
+    public BoolVal(boolean place) {
+	     boolean bool = place;
+    }
+
+    public String toString() {
+	     return String.valueOf(bool);
+    }
+}
